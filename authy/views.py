26c7dfae4d20d0b44d7db4186b3a8055eedf43ae
Your Name <you@example.com>
2021-07-25 16:25:28 +0300

    created update user profile function

--- authy/views.py
+++ authy/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm
+from .forms import CreateUserForm,UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from main.views import home
@@ -42,4 +42,22 @@ def logoutUser(request):
     return redirect('login')
 
 def Profile(request):
-    return render(request,'profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance = request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES, instance = request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.info(request, 'Your details have been updated')
+            return redirect('profile')
+    else:
+         u_form = UserUpdateForm(instance = request.user)
+         p_form = ProfileUpdateForm(instance = request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request,'profile.html',context)
+
