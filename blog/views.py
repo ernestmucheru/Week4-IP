d47911ebfa669b4682307321dea48d273a5aebba
Your Name <you@example.com>
2021-07-25 19:34:10 +0300

    create a  post delete view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import (ListView,DetailView,CreateView,UpdateView)
+from django.views.generic import (ListView,DetailView,CreateView,UpdateView,DeleteView)
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -32,6 +32,14 @@ class PostUpdateView(LoginRequiredMixin, UpdateView,  UserPassesTestMixin):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+class PostDeleteView(LoginRequiredMixin, DeleteView,  UserPassesTestMixin):
+    model = Post
+    success_url = '/'
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
