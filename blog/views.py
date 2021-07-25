f4fa50483d220d2714074c56043df19c20c74949
Your Name <you@example.com>
2021-07-25 19:20:23 +0300

    created a post update function

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import (ListView,DetailView,CreateView)
+from django.views.generic import (ListView,DetailView,CreateView,UpdateView)
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 def blog(request):
@@ -17,10 +18,22 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title','content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+class PostUpdateView(LoginRequiredMixin, UpdateView,  UserPassesTestMixin):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
