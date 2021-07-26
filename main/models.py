9f4d20e49bb6745692f18f6320ffa4dd4fab176a
Your Name <you@example.com>
2021-07-26 05:57:18 +0300

    added a neighbourhood model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=250)
+    location = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='hood_images/', blank=True)
+
+    class Meta:
+        db_table = 'neighbourhoods'
+        ordering = ['-name']
+
+    def __repr__(self):
+        return f'{self.name}'
+
+    def __str__(self):
+        return f"{self.name}"
+
+    @classmethod
+    def search_hood(cls, searchTerm):
+        hoods = cls.objects.filter(name__icontains=searchTerm)
+        return hoods
+
+    @classmethod
+    def search_by_title(cls, search_term):
+        hoods = cls.objects.filter(name__icontains=search_term)
+        return hoods
+        
