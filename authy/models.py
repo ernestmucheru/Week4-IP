b0d58ea423e249f59924c6d92feac02a59c6c5ba
Your Name <you@example.com>
2021-07-26 06:55:25 +0300

    changes

--- authy/models.py
+++ authy/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from main.models import Neighbourhood
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-
+    age = models.IntegerField(null=True)
+    contact = models.CharField(max_length=250)
+    address = models.CharField(max_length=250)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, blank=True, null=True)
+    
     def __str__(self):
         return f'{self.user.username} Profile'
     def save(self):
