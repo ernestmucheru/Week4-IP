0401e975da639db9cf6d9f0e5c0ced9af5d4dbb4
Your Name <you@example.com>
2021-07-26 06:12:03 +0300

    added class methods to my models

--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,21 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        db_table = 'posts'
+        ordering = ['-title']
+        
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+    
+
+    def __repr__(self):
+        return f'{self.title}'
+
+    @classmethod
+    def search_posts(cls, searchTerm):
+        posts = cls.objects.filter(title__icontains=searchTerm)
+        return posts
